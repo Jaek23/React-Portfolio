b7dbc0bdcf3028510ae035ebc909d7d4f947aab9
jae kim <jaehkim23@gmail.com>
2023-12-14 21:01:26 -0600

    updated resume

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,7 @@ const About = () =>{
                 <h1 className="card-title">About Me</h1>
                 <p className="card-text">Aspiring software engineer with a background in marketing and sales. I completed the Full Stack Web Development Bootcamp at the University of Texas at Austin in 2023. I enjoy problem solving and creating user-friendly and visually appealing applications. I am continuously learning new skills to advance my full-stack developing skills. My skills include HTML, CSS, JavaScript, React, Express, Node, and Git.</p>
                 <a 
-                href="https://docs.google.com/document/d/13SJRFSt4HOCTzRtZ-Q4Gg6UaP1j1dmzT/edit"
+                href="https://docs.google.com/document/d/17wcc7avjUgzA59OJ9VIEKJ4Don3TxAvFJJZCxvOGVMw/edit"
                 className='resume-btn'
                 target='null'
                 >
